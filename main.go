4cb731eab0133f654acf3cc2d2a3258a7d3f8f45
Sigurd Rosland <Sigurd Rosland>
2016-04-17 20:03:56 +0100

    Rename newq

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/knutaldrin/elevator/driver"
 	"github.com/knutaldrin/elevator/log"
 	"github.com/knutaldrin/elevator/net"
-	"github.com/knutaldrin/elevator/newq"
+	"github.com/knutaldrin/elevator/queue"
 )
 
 // fuck main
@@ -32,7 +32,7 @@ func main() {
 	}
 
 	fmt.Println("Id:", *id)
-	newq.SetID(*id)
+	queue.SetID(*id)
 
 	currentDirection := driver.DirectionNone
 	lastFloor := driver.Floor(0)
@@ -42,10 +42,10 @@ func main() {
 	// Init driver and make sure elevator is at a floor
 	driver.Init()
 
-	newq.ImportInternalLog()
+	queue.ImportInternalLog()
 
 	lastFloor = driver.Reset()
-	newq.Update(lastFloor)
+	queue.Update(lastFloor)
 
 	floorCh := make(chan driver.Floor)
 	go driver.FloorListener(floorCh)
@@ -60,7 +60,7 @@ func main() {
 	go net.InitAndHandle(orderReceiveCh, *id)
 
 	timeoutCh := make(chan bool, 8)
-	newq.SetTimeoutCh(timeoutCh)
+	queue.SetTimeoutCh(timeoutCh)
 
 	// Oh, God almighty, please spare our ears
 	sigtermCh := make(chan os.Signal)
@@ -75,10 +75,10 @@ func main() {
 	for {
 		select {
 		case fl := <-floorCh:
-			newq.Update(fl)
-			if newq.ShouldStop(fl) {
+			queue.Update(fl)
+			if queue.ShouldStop(fl) {
 				driver.Stop()
-				newq.ClearOrderLocal(fl, currentDirection)
+				queue.ClearOrderLocal(fl, currentDirection)
 				log.Debug("Stopped at floor ", fl)
 				net.SendOrder(net.OrderMessage{Type: net.CompletedOrder, Floor: fl, Direction: currentDirection})
 
@@ -88,18 +88,18 @@ func main() {
 					time.Sleep(1 * time.Second)
 					doorOpen = false
 					driver.CloseDoor()
-					currentDirection = newq.NextDirection()
+					currentDirection = queue.NextDirection()
 					driver.Run(currentDirection)
 				}()
 			}
 
 		case btn := <-floorBtnCh:
-			newq.NewOrder(btn.Floor, btn.Dir)
+			queue.NewOrder(btn.Floor, btn.Dir)
 			if btn.Dir != driver.DirectionNone {
 				net.SendOrder(net.OrderMessage{Type: net.NewOrder, Floor: btn.Floor, Direction: btn.Dir})
 			}
 			if !doorOpen {
-				currentDirection = newq.NextDirection()
+				currentDirection = queue.NextDirection()
 				driver.Run(currentDirection)
 			}
 
@@ -107,19 +107,19 @@ func main() {
 			switch o.Type {
 			case net.NewOrder:
 				log.Debug("New order, floor: ", o.Floor, ", dir: ", o.Direction)
-				newq.NewOrder(o.Floor, o.Direction)
+				queue.NewOrder(o.Floor, o.Direction)
 
 			case net.AcceptedOrder:
 				log.Debug("Remote accepted order, floor: ", o.Floor, ", dir: ", o.Direction)
-				newq.OrderAcceptedRemotely(o.Floor, o.Direction)
+				queue.OrderAcceptedRemotely(o.Floor, o.Direction)
 
 			case net.CompletedOrder:
 				log.Debug("Remote completed order, floor: ", o.Floor, ", dir: ", o.Direction)
-				newq.ClearOrder(o.Floor, o.Direction)
+				queue.ClearOrder(o.Floor, o.Direction)
 			}
 
 		case <-timeoutCh:
-			currentDirection = newq.NextDirection()
+			currentDirection = queue.NextDirection()
 			if !doorOpen {
 				driver.Run(currentDirection)
 			}
